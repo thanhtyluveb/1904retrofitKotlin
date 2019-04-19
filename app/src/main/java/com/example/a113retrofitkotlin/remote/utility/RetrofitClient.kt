cba2c962a07d99b7a7151ea20a0d205860edc8b3
Ngoc <thanhtyluven@gmail.com>
2019-04-19 15:16:48 +0700

    1904 addInterceptor

--- app/src/main/java/com/example/a113retrofitkotlin/remote/utility/RetrofitClient.kt
+++ app/src/main/java/com/example/a113retrofitkotlin/remote/utility/RetrofitClient.kt
@@ -1,29 +1,45 @@
 package com.example.recyclerview.remote.retrofit
 
-import java.util.concurrent.TimeUnit
-
+import okhttp3.Credentials
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
     private var retrofit: Retrofit? = null
 
     fun getClient(baseUrl: String): Retrofit? {
-        val buider = OkHttpClient.Builder()
-                .readTimeout(5000, TimeUnit.MILLISECONDS)
-                .writeTimeout(5000, TimeUnit.MILLISECONDS)
-                .connectTimeout(10000, TimeUnit.MILLISECONDS)
-                .retryOnConnectionFailure(true)
-                .build()
+
+        var buider = OkHttpClient().newBuilder().addInterceptor { chain ->
+            val originalRequest = chain.request()
+
+            val builder = originalRequest.newBuilder().header(
+                "Authorization",
+                Credentials.basic("aUsername", "aPassword")
+            )
+
+            val newRequest = builder.build()
+            chain.proceed(newRequest)
+
+        }.readTimeout(5000, TimeUnit.MILLISECONDS)
+            .writeTimeout(5000, TimeUnit.MILLISECONDS)
+            .connectTimeout(10000, TimeUnit.MILLISECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
+
+
+
+
         if (retrofit == null) {
             retrofit = Retrofit.Builder()
-                    .client(buider)
-                    .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
+                .client(buider)
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
         }
         return retrofit
     }
+
 }
